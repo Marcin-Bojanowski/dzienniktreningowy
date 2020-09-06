b53672b33b43bbebd07f9fa094d5d15b994af750
Marcin-Bojanowski <bojanowski.ma@gmail.com>
2020-09-06 12:54:04 +0200

    first commit

---
+++ src/main/java/pl/coderslab/dzienniktreningowy/DzienniktreningowyApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.dzienniktreningowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzienniktreningowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DzienniktreningowyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/dzienniktreningowy/DzienniktreningowyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.dzienniktreningowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzienniktreningowyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
